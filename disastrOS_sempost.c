205ed40009b80c34a1b66c7dd7ea7d3d9d1cafbd
luciatesta97 <56517946+luciatesta97@users.noreply.github.com>
2019-11-13 20:04:44 +0100

    sem_post & test

--- disastrOS_sempost.c
+++ disastrOS_sempost.c
@@ -7,5 +7,31 @@
 #include "disastrOS_semdescriptor.h"
 
 void internal_semPost(){
-  // do stuff :)
+  int fd = running -> syscall_args[0]; 
+
+	SemDescriptor *sem_desc = SemDescriptorList_byFd(&running -> sem_descriptors, fd);
+    
+    //validation of sem_desc
+	if (!sem_desc) {
+        running -> syscall_retvalue = DSOS_EINVAL; 
+        return;
+    }
+
+   
+     
+    // process blocked in a sem_wait call will be woken
+    if (sem_desc -> semaphore -> count == 0 && sem_desc -> semaphore -> waiting_descriptors.size) {
+        SemDescriptorPtr *sem_desc_ptr = (SemDescriptorPtr*) List_detach(&sem_desc -> semaphore -> waiting_descriptors, sem_desc -> semaphore -> waiting_descriptors.first);
+        
+        //taking PCB of process that was in waiting list
+        PCB *resume = sem_desc_ptr -> descriptor -> pcb;
+        resume = (PCB*) List_detach(&waiting_list, (ListItem*) resume);
+        resume -> status = Ready;
+        //So the process is now ready 
+        List_insert(&ready_list, ready_list.last, (ListItem*) resume);
+
+        SemDescriptorPtr_free(sem_desc_ptr);
+        running -> syscall_retvalue =  0;
+        return;
+    }
 }
