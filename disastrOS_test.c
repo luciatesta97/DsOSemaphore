0080b7e4a867deb74a27b21632e21e4dd67bb9bc
luciatesta97 <56517946+luciatesta97@users.noreply.github.com>
2019-11-14 19:45:05 +0100

    Test on all semaphores syscalls.
    Creating semaphores, define number children. 
    Redifine functions. 
    Debug.

--- disastrOS_test.c
+++ disastrOS_test.c
@@ -3,9 +3,16 @@
 #include <poll.h>
 
 #include "disastrOS.h"
-//handling errors' codes 
+#include "disastrOS_semaphore.h"
+
+#define CHILDREN 10
+#define NRES 3
+
+int Global_Resources[NRES];
+int Semaphores_fd[NRES];
+
 void sem_errors(int err_code) {
-    //if error code not negative, OK!
+
     if (err_code >= 0) {
         printf("\e[32mSuccess\e[0m\n");
         return;
@@ -41,66 +48,199 @@ void sleeperFunction(void* args){
   }
 }
 
-void childFunction(void* args){
-  printf("Hello, I am the child function %d\n",disastrOS_getpid());
-  printf("I will iterate a bit, before terminating\n");
-  int type=0;
-  int mode=0;
-  int fd=disastrOS_openResource(disastrOS_getpid(),type,mode);
-  printf("fd=%d\n", fd);
-  printf("PID: %d, terminating\n", disastrOS_getpid());
+int pid_map(int pid){
+    // We subtract two because the first
+    // two pids in this example are "init" and "sleeper"
+    //so we start from third one 
+    //0 if we have pid multiple NRES
+    return (pid -2) % NRES;
+}
 
-  for (int i=0; i<(disastrOS_getpid()+1); ++i){
-    printf("PID: %d, iterate %d\n", disastrOS_getpid(), i);
-    disastrOS_sleep((20-disastrOS_getpid())*5);
-  }
-  disastrOS_exit(disastrOS_getpid()+1);
+void print_GR() {
+    printf("Global Resources: ");
+    for (int i = 0; i < NRES; i++)
+        printf("%d ", Global_Resources[i]); 
+    printf("\n");
+}
+
+//managing child with semaphores
+void main_child(){
+    int pid = disastrOS_getpid();
+    int map = pid_map(pid);
+
+    //As Id of semaphore we have process Map 
+    // 0 as flag
+    // 0 as value
+
+    int fd = disastrOS_semOpen(map, 0, 0);
+
+    int r;
+    printf("\e[36m[%d]\e[0m WAIT ON SEMAPHORE %d \n", pid, map);
+    r = disastrOS_semWait(fd);
+    sem_errors(r);
+    
+
+    //printing global resources
+    for (int i = 0; i < 10; ++i) {
+        //process id, sem id <- map 
+        printf("\e[36m[%d]\e[0m ADD TO GLOBAL_RESOURCES[%d]\n", pid, map);
+        Global_Resources[map] ++;
+        print_GR();
+        disastrOS_sleep(3);
+    }
+
+    printf("\e[36m[%d]\e[0m POST ON SEMAPHORE %d \n", pid, map);
+    r = disastrOS_semPost(fd);
+    sem_errors(r);
+
+    printf("\e[36m[%d]\e[0m CLOSING SEMAPHORE %d \n", pid, map);
+    r = disastrOS_semClose(fd);
+    sem_errors(r);
+
+    disastrOS_exit(0);
 }
 
 
 void initFunction(void* args) {
-  disastrOS_printStatus();
-  printf("hello, I am init and I just started\n");
-  disastrOS_spawn(sleeperFunction, 0);
-  
-
-  printf("I feel like to spawn 10 nice threads\n");
-  int alive_children=0;
-  for (int i=0; i<10; ++i) {
-    int type=0;
-    int mode=DSOS_CREATE;
-    printf("mode: %d\n", mode);
-    printf("opening resource (and creating if necessary)\n");
-    int fd=disastrOS_openResource(i,type,mode);
-    printf("fd=%d\n", fd);
-    disastrOS_spawn(childFunction, 0);
-    alive_children++;
-  }
-
-  disastrOS_printStatus();
-  int retval;
-  int pid;
-  while(alive_children>0 && (pid=disastrOS_wait(0, &retval))>=0){ 
     disastrOS_printStatus();
-    printf("initFunction, child: %d terminated, retval:%d, alive: %d \n",
-	   pid, retval, alive_children);
-    --alive_children;
-  }
-  printf("shutdown!");
-  disastrOS_shutdown();
+    printf("hello, I am init and I just started\n");
+    disastrOS_spawn(sleeperFunction, 0);
+
+    int ret, fd, fd1;
+    printf("SEMAPHORES:\n");
+    //ID: 0
+    //FLAG:0
+    //VALUE: 1
+    printf("Open uninitialized Sem. 0 without CREATE, with value 1:\n");
+    ret = disastrOS_semOpen(0, 0, 1);
+    sem_errors(ret);
+
+    printf("Open Sem. 0 with value 1 (EXCL):\n");
+    ret = disastrOS_semOpen(0, DSOS_CREATE | DSOS_EXCL, 1);
+    fd = ret;
+    sem_errors(ret);
+    disastrOS_printStatus();
+
+    printf("Reopen Sem. 0 with value 1 (Not EXCL):\n");
+    ret = disastrOS_semOpen(0, DSOS_CREATE, 2);
+    fd1 = ret;
+    sem_errors(ret);
+    disastrOS_printStatus();
+
+    printf("Close Sem Ptr created with previous syscall:\n");
+    ret = disastrOS_semClose(fd1);
+    sem_errors(ret);
+    disastrOS_printStatus();
+
+    printf("Reopen Sem. 0 with value 1 (EXCL):\n");
+    ret = disastrOS_semOpen(0, DSOS_CREATE | DSOS_EXCL, 1);
+    sem_errors(ret);
+
+    printf("Post on uninitialized semaphore:\n");
+    ret = disastrOS_semPost(32);
+    sem_errors(ret);
+
+    printf("Wait on uninitialized semaphore:\n");
+    ret = disastrOS_semWait(32);
+    sem_errors(ret);
+
+    printf("Close Sem. 0:\n");
+    ret = disastrOS_semClose(fd);
+    sem_errors(ret);
+    disastrOS_printStatus();
+
+    printf("Reclose Sem. 0:\n");
+    ret = disastrOS_semClose(fd);
+    sem_errors(ret);
+
+    printf("Open sem. -5\n");
+    ret = disastrOS_semOpen(-5, DSOS_CREATE | DSOS_EXCL, 1);
+    fd = ret;
+    sem_errors(ret);
+    disastrOS_printStatus();
+
+    printf("Close sem. -5\n");
+    ret = disastrOS_semClose(fd);
+    sem_errors(ret);
+    disastrOS_printStatus();
+
+    //printf("Open sem. 2000\n");
+    //r = disastrOS_semOpen(2000, DSOS_CREATE | DSOS_EXCL, 1);
+    //fd = r;
+    //sem_errors(r);
+    //disastrOS_printStatus();
+
+    //printf("Close sem. 2000\n");
+    //r = disastrOS_semClose(fd);
+    //sem_errors(r);
+    //disastrOS_printStatus();
+
+
+    int fds[MAX_NUM_SEMAPHORES+1];
+    printf("Open %d semaphores (1 over limit)\n", MAX_NUM_SEMAPHORES+1);
+    for (int i = 0; i < MAX_NUM_SEMAPHORES+1; ++i) {
+        ret = disastrOS_semOpen(i, DSOS_CREATE | DSOS_EXCL, 1);
+        fds[i] = ret;
+        sem_errors(ret);
+    }
+
+    printf("Close %d semaphores (even the extra one)\n", MAX_NUM_SEMAPHORES+1);
+    for (int i = 0; i < MAX_NUM_SEMAPHORES+1; ++i) {
+        ret = disastrOS_semClose(fds[i]);
+        sem_errors(ret);
+    }
+
+    printf("________________________________\n");
+
+
+    printf("\e[36m[init]\e[0m Initialize sems with ids from 0 to %d, all at value 1\n", NRES-1);
+    for (int i = 0; i < NRES; ++i) {
+        ret = disastrOS_semOpen(i, DSOS_CREATE | DSOS_EXCL, 1);
+        sem_errors(ret);
+        Semaphores_fd[i] = ret;
+    }
+
+    disastrOS_printStatus();
+
+    printf("\e[36m[init]\e[0m Creating %d children\n", CHILDREN);
+    for (int i = 0; i < CHILDREN; ++i)
+        disastrOS_spawn(main_child, 0);
+
+    disastrOS_printStatus();
+
+    int retval;
+    int pid;
+    printf("\e[36m[init]\e[0m Wait for all children\n");
+    for (int i = 0; i < CHILDREN; ++i) {
+        pid = disastrOS_wait(0, &retval);
+        printf("\e[36m[init]\e[0m child %d returned with code: %d\n", pid, retval);
+        disastrOS_printStatus();
+    }
+
+    disastrOS_printStatus();
+
+    printf("\e[36m[init]\e[0m Close all sems.\n");
+    for (int i = 0; i < NRES; ++i) {
+        ret = disastrOS_semClose(Semaphores_fd[i]);
+        sem_errors(ret);
+    }
+
+    disastrOS_printStatus();
+
+    printf("SHUTDOWN!");
+    disastrOS_shutdown();
 }
 
 int main(int argc, char** argv){
   char* logfilename=0;
+
   if (argc>1) {
     logfilename=argv[1];
   }
-  // we create the init process processes
-  // the first is in the running variable
-  // the others are in the ready queue
-  printf("the function pointer is: %p", childFunction);
+
   // spawn an init process
   printf("start\n");
   disastrOS_start(initFunction, 0, logfilename);
+
   return 0;
 }
